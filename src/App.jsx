ba3e46a6ea93005615408dab65314d164dd6ea89
aaronmuli <83603318+aaronmuli@users.noreply.github.com>
2023-06-07 05:00:18 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"
 
 function App() {
@@ -18,7 +17,6 @@ function App() {
         <Route path="/register" element={<Register/>}/>
       </Routes>
     </Router>
-    <ToastContainer/>
     </>
   );
 }
